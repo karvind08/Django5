cf542e5a4959799f9027930dfc17d93114c72fd8
karvind08 <karvind08@gmail.com>
2025-02-26 13:51:30 +0530

    26 February 2025

--- app2/views.py
+++ app2/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def app2fun(request):
-    return HttpResponse('<h2>Messgae from App2 Fun</h2>')
+    html = '<h2>Messgae from App2 Fun with variable</h2>'
+    # return HttpResponse('<h2>Messgae from App2 Fun</h2>')
+    return HttpResponse(html)
