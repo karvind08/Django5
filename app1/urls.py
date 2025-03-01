96a4f5d8e3a160e6ce5f11bda2e81575b5a8ac62
karvind08 <karvind08@gmail.com>
2025-03-01 13:11:30 +0530

    1 March 2025

--- app1/urls.py
+++ app1/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from app1 import views
 urlpatterns = [
-    path('',views.home,name='Home'),
+    path('show/',views.show,name='show'),
+    path('home/',views.home,name='home'),
     
 ]
--- app1/views.py
+++ app1/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def show(request):
+    return render(request,'app1/show.html')
+
 def home(request):
-    return HttpResponse('Home Page of App1')
+    return render(request,'app1/home.html')
