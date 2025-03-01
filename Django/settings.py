8bda924bd5d48d80bb3c52c651cca8f4a84f977c
karvind08 <karvind08@gmail.com>
2025-03-01 18:01:17 +0530

    1 March 2025

--- Django/settings.py
+++ Django/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
     'app1',
     'app2',
+    
 ]
 
 MIDDLEWARE = [
