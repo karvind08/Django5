492db2752e4c2ec511c59f53553a393584b73615
karvind08 <karvind08@gmail.com>
2025-03-03 22:47:11 +0530

    3 March 2025

--- app1/models.py
+++ app1/models.py
@@ -6,3 +6,4 @@ class Profile(models.Model):
     email = models.EmailField(max_length=255)
     city = models.CharField(max_length=70)
     roll = models.IntegerField()
+    course = models.CharField(max_length=50)
